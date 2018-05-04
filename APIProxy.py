25272b39f289b387fb7feb4fee410a6780e30067
Gregory Short <glshort@alaska.edu>
2018-05-04 16:20:55 -0500

    EB was looking for application, not app

--- APIProxy.py
+++ APIProxy.py
@@ -1,5 +1,6 @@
 from flask import Response
 import requests
+import logging
 
 class APIProxy(object):
     
@@ -8,4 +9,6 @@ class APIProxy(object):
 
     def get_response(self):
         r = requests.get('https://api.daac.asf.alaska.edu/services/search/param?api_proxy=1&{0}'.format(self.request.query_string))
+        if r.status_code == 500:
+            app.logger.warning('Received status_code 500 from ASF API!')
         return Response(r.text, r.status_code, r.headers.items())
