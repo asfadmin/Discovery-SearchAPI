6869b882a19dda043dccc3139b51f6bb08328687
Gregory Short <glshort@alaska.edu>
2018-05-04 16:59:04 -0500

    Remove extra logging statement

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ application = Flask(__name__)
 # Send the generated script as an attachment so it downloads directly
 @application.route('/services/search/param', methods = ['GET', 'POST'])
 def proxy_search():
-    application.logger.debug('API passthrough from {0} with params {1}'.format(request.access_route[-1], request.query_string))
     api = APIProxy(request)
     return api.get_response()
 
