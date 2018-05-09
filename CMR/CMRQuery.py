902c101a600fb57a0d8161a6df1846af3b28af94
Gregory Short <glshort@alaska.edu>
2018-05-08 21:26:00 -0500

    Started support for subqueries, for parallelization and more parameter support

--- CMR/CMRQuery.py
+++ CMR/CMRQuery.py
@@ -8,7 +8,6 @@ from Analytics import post_analytics
 class CMRQuery:
     
     def __init__(self, params=None, max_results=None, output='metalink'):
-        self.echo10_results = []
         self.params = params
         self.max_results = max_results
         self.output = output
@@ -20,39 +19,63 @@ class CMRQuery:
         # minimize data transfer if all we need is the hits header
         if self.output == 'count':
             self.params['page_size'] = 1
+            r = requests.post(urls.cmr_api, data=self.params)
+        
+            # forward anything other than a 200
+            if r.status_code != 200:
+                logging.warning('Non-200 response from CMR, forwarding to client')
+                post_analytics(r, 'Proxy Error', 'CMR API {0}'.format(r.status_code))
+                return Response(r.text, r.status_code, r.header_items())
+        
+            hits = int(r.headers['CMR-hits'])
+            return make_response('{0}'.format(hits))
+        
+        if self.output == 'echo10': #truncate echo10 output to 1 page
+            self.max_results = min(self.max_results, self.params['page_size'])
+            
+        subq = CMRSubQuery(params=self.params, max_results=self.max_results)
+        results = subq.get_results()
+        
+        # trim the results if needed
+        if self.max_results is not None and len(results) > self.max_results:
+            logging.debug('trimming total results from {0} to {1}'.format(len(results), self.max_results))
+            results = results[0:self.max_results]
+        return make_response(translators.get(self.output, translators['metalink'])(results))
+
+class CMRSubQuery:
+    
+    def __init__(self, params, max_results=None):
+        self.params = params
+        self.max_results = max_results
+        logging.debug('new CMRSubQuery object ready to go')
+    
+    def get_results(self):
+        logging.debug('fetching subquery results')
         
         r = requests.post(urls.cmr_api, data=self.params)
         
         # forward anything other than a 200
         if r.status_code != 200:
-            logging.warning('Non-200 response from CMR, forwarding to client')
-            post_analytics(r, 'Proxy Error', 'CMR API {0}'.format(r.status_code))
-            return Response(r.text, r.status_code, r.header_items())
-        
+            return [] # do something wiser here
+            
         hits = int(r.headers['CMR-hits'])
         sid = r.headers['CMR-Scroll-Id']
         
-        # intercept count option
-        if self.output == 'count':
-            return make_response(hits)
-            
         segment = parse_cmr_response(r)
         results = []
             
-        # fetch multiple pages of results if needed, unless it's echo10 output, then stop after 1 page
+        # fetch multiple pages of results if needed
         while True:
             results.extend(segment)
             logging.debug('fetched {0}/{1}'.format(len(results), min(hits, self.max_results)))
-            if len(segment) <= 0 \
-               or (self.max_results is not None and len(results) >= self.max_results) \
-               or self.output == 'echo10':
+            if len(segment) <= 0 or (self.max_results is not None and len(results) >= self.max_results):
                 break
             r = requests.post(urls.cmr_api, data=self.params, headers={'CMR-Scroll-Id': sid})
             segment = parse_cmr_response(r)
         
         # trim the results if needed
-        if self.max_results is not None and len(results) >= self.max_results:
-            logging.debug('trimming results from {0} to {1}'.format(len(results), self.max_results))
+        if self.max_results is not None and len(results) > self.max_results:
+            logging.debug('trimming subquery results from {0} to {1}'.format(len(results), self.max_results))
             results = results[0:self.max_results]
-        return make_response(translators.get(self.output, translators['metalink'])(results))
         
+        return results
