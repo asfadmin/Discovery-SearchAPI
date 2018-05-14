3d5cc7bdfab8728e2a77d57b920f18a72ab38670
Gregory Short <glshort@alaska.edu>
2018-05-14 12:30:27 -0500

    Add dev- and test-specific analytics tracking IDs

--- asf_env.py
+++ asf_env.py
@@ -8,13 +8,13 @@ def get_config():
             'asf_api': 'https://api-legacy.daac.asf.alaska.edu/services/search/param',
             'cmr_api': 'https://cmr.uat.earthdata.nasa.gov/search/granules.echo10',
             'bulk_download_api': 'https://bulk-download-test.asf.alaska.edu',
-            'analytics_id': None
+            'analytics_id': 'UA-118881300-4'
         },
         'test': {
             'asf_api': 'https://api-legacy.daac.asf.alaska.edu/services/search/param',
             'cmr_api': 'https://cmr.uat.earthdata.nasa.gov/search/granules.echo10',
             'bulk_download_api': 'https://bulk-download-test.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-2'
+            'analytics_id': 'UA-118881300-3'
         },
         'prod': {
             'asf_api': 'https://api-legacy.daac.asf.alaska.edu/services/search/param',
