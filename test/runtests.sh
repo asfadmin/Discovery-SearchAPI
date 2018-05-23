bdceae88e5d92ec55386c4658a0a5543ae52a7b9
Gregory Short <glshort@alaska.edu>
2018-05-22 22:21:10 -0500

    Testing shortcut bash script, and cleanup

---
+++ test/runtests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Shortcut for running tester.py with some default options on various maturities
+if [ $1 = 'test' ]
+then
+    api="https://api-test.asf.alaska.edu/services/search/param"
+elif [ $1 = 'prod' ]
+then
+    api="https://api.daac.asf.alaska.edu/services/search/param"
+else
+    api="http://127.0.0.1:5000/services/search/param"
+fi
+echo "Testing maturity $1"
+echo "API URL $api"
+n="$(egrep '^\s*http' queries.txt | wc -l | egrep -o '\d+')"
+echo "Running $n tests"
+python tester.py -f queries.txt -s results.csv -c -r "$api"
