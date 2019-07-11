e797ad55d91a731c01c87c24acaa7c9cc471c044
Gregory Short <glshort@alaska.edu>
2019-07-11 17:26:54 -0500

    Add the error into the health check output

--- CMR/Health.py
+++ CMR/Health.py
@@ -5,6 +5,10 @@ from asf_env import get_config
 
 def get_cmr_health():
     cfg = get_config()
-    r = requests.get(cfg['cmr_base'] + cfg['cmr_health'], timeout=10)
-    d = {'host': cfg['cmr_base'], 'health': json.loads(r.text)}
+    try:
+        r = requests.get(cfg['cmr_base'] + cfg['cmr_health'], timeout=10)
+        d = {'host': cfg['cmr_base'], 'health': json.loads(r.text)}
+    except Exception as e:
+        logging.debug(e)
+        d = {'host': cfg['cmr_base'], 'error': '{0}'.format(e)}
     return d
