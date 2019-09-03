9c5bcae03731e3b2f0b146f6073a69e0ecf26fe7
Cameron Showalter <cjshowalter@alaska.edu>
2019-09-03 13:36:32 -0800

    Fixed featureCollectionGeometry_2 test, added notes for future tests

--- test_cameron/test_FilesToWKT.py
+++ test_cameron/test_FilesToWKT.py
@@ -32,85 +32,44 @@ class Test_parseGeoJson():
         geojson_path = os.path.join(self.resources_root, "featureCollectionGeometry_1.geojson")
         file = open(geojson_path, "r")
         wkt = test_file.parse_geojson(file)
+        # print("---->>> " + str(wkt))
         assert wkt["wkt"]["wrapped"] == 'POLYGON ((100.0000000000000000 0.0000000000000000, 101.0000000000000000 0.0000000000000000, 101.0000000000000000 1.0000000000000000, 100.0000000000000000 1.0000000000000000, 100.0000000000000000 0.0000000000000000))'
         assert wkt["wkt"]["unwrapped"] == 'POLYGON ((100.0000000000000000 0.0000000000000000, 101.0000000000000000 0.0000000000000000, 101.0000000000000000 1.0000000000000000, 100.0000000000000000 1.0000000000000000, 100.0000000000000000 0.0000000000000000))'
 
 
     def test_featureCollectionGeometry_2(self):
-
         geojson_path = os.path.join(self.resources_root, "featureCollectionGeometry_2.geojson")
         file = open(geojson_path, "r")
-        print(test_file.parse_geojson(file))
-        # geojson = json.loads(file.read())
+        wkt = test_file.parse_geojson(file)
+        # print("---->>> " + str(wkt))
+        assert wkt["wkt"]["wrapped"] == 'POLYGON ((100.0000000000000000 0.0000000000000000, 104.0000000000000000 0.0000000000000000, 105.0000000000000000 1.0000000000000000, 100.0000000000000000 1.0000000000000000, 100.0000000000000000 0.0000000000000000))'
+        assert wkt["wkt"]["unwrapped"]== 'POLYGON ((100.0000000000000000 0.0000000000000000, 104.0000000000000000 0.0000000000000000, 105.0000000000000000 1.0000000000000000, 100.0000000000000000 1.0000000000000000, 100.0000000000000000 0.0000000000000000))'
 
-        # # Add all geometries you find to a list. Could be nested deep inside json:
-        # geometry_objs = []
-
-        # # Geometry is just one object
-        # for geom in find_elements(geojson, "geometry"):
-        #     geometry_objs.append(geom)
-
-        # # Geometries is a list of objects
-        # for geometries in find_elements(geojson, "geometries"):
-        #     for geom in geometries:
-        #         geometry_objs.append(geom)
-
-
-        # if len(geometry_objs) == 0:
-        #     return "Could not find any geometries to convert to wkt."
-        # elif len(geometry_objs) == 1:
-        #     wkt_str = wkt.dumps(geometry_objs[0])
-        #     return repairWKT(wkt_str)
-        # else:  # len(geometry_objs) > 1
-
-        #     # Combine all the points of each object into one:
-        #     all_coords = "["
-        #     for geom in geometry_objs:
-        #         match_coords = r'(\[\s*((\d+\.+\d*)|(\d*\.+\d+)|(\d+))\s*,\s*((\d+\.+\d*)|(\d*\.+\d+)|(\d+))\s*\])'
-        #         cords = re.findall(match_coords,str(geom["coordinates"]))
-        #         for i, cord in enumerate(cords):
-        #             # Not sure why this is 2D, maybe a regex fix here.
-        #             all_coords += str(cords[i][0]) + ","
-        #     # Take off the last cooma, add the last brace:
-        #     all_coords = str(all_coords)[0:-1] + "]"
-        #     wkt_json = '{"type": "MultiPoint", "properties": {}, "coordinates": ' + all_coords + '}'
-        #     wkt_json = json.loads(wkt_json)
-        #     # print(shapely.wkt.loads(wkt_json))
-        #     # print(wkt_json)
-        #     print(shapely.geometry.shape(wkt_json).convex_hull)
-        #     return
-        #     print(shape(wkt_json))
-
-        #     shape = shapely.wkt.loads(wkt.dumps(wkt_obj)).convex_hull
-
-            #     # print(all_coords)
-            # return
-
-
-
-
-
-            # # Throw everything into a single geometric collection, and convex_hall it:
-            # wkt_json = '{"type": "GeometryCollection","geometries": ['
-            # for geometry in geometry_objs:
-            #     wkt_json += json.dumps(geometry) + ','
-            # # take out last ',' then finish the json object:
-            # wkt_json = wkt_json[:-1] + ']}'
-            # wkt_json = json.loads(wkt_json)
-            # print(shape(wkt_json))
-            # # print(wkt.dumps(wkt_json))
-
-            # return
-            # print(wkt_json)
-        # Now you have all the geometries from file. Convert to wkt:
-
-        # wkt = test_file.parse_geojson(file)
-
-
-
-    # def test_featureCollectionGeometryCollection(self):
+    ## This test fails because the "polygon" only has two points, and throws an error when
+    ## parsed inside repairWKT. Should repairWKT handle this case, or should it be fixed
+    ## inside parse_geojson? (Ideal fix be converting to line and continuing w/ warning?)
+    # def test_featureCollectionGeometryCollection_1(self):
     #     file_path = os.path.join(self.resources_root, "featureCollectionGeometryCollection_1.geojson")
     #     file = open(file_path, "r")
-    #     # data = json.loads(file.read())['features'][0]['geometry']
-    #     data = test_file.parse_geojson(file)
-    #     print(data)
+    #     wkt = test_file.parse_geojson(file)
+    #     print(wkt)
+
+    # TODO: add test for single geom with "MultiPolygon" type. Currently gets passed into repairWKT.
+    # Should maybe covert to multipoint and call convex_hull? Fix in parse_geojson or repairWKT? 
+
+    # TODO: add test for geojson NOT containing geometry tag, just type/coords
+    # ie. is this a valid geojson?:
+    # {
+    #     "type": "MultiPolygon", 
+    #     "coordinates": [
+    #         [
+    #             [[40, 40], [20, 45], [45, 30], [40, 40]]
+    #         ], 
+    #         [
+    #             [[20, 35], [10, 30], [10, 10], [30, 5], [45, 20], [20, 35]], 
+    #             [[30, 20], [20, 15], [20, 25], [30, 20]]
+    #         ]
+    #     ]
+    # }
+    # easyist fix, wrap everything in { "geometry": file.read() }. Method already handles if
+    # geom has no "coordinates" field, and skips over it
