0eb10172190c330200a5e53afccb39abace3881b
Cameron Showalter <cjshowalter@alaska.edu>
2019-11-04 17:43:46 -0900

    Added code for formating returned API file, and checking results are what url asked for. Hooked up absoluteOrbit and platform to this so far.

--- test/test_URLs.py
+++ test/test_URLs.py
@@ -1,49 +1,171 @@
 
-import os, sys, yaml, json, re
+import os, sys, yaml, re
+from io import StringIO
+import csv
+from csv import Error
+import json
+from json import JSONDecodeError
 import pytest, warnings
 import requests
 import glob
 import itertools
 import argparse
+from copy import deepcopy
 
 # Let python discover other modules, starting one dir behind this one (project root):
 project_root = os.path.realpath(os.path.join(os.path.dirname(__file__),".."))
 sys.path.insert(0, project_root)
 import conftest as helpers
+from CMR import Input
 
 class RunSingleURLFromFile():
     def __init__(self, json_dict, url_api):
+        self.query, assert_used = self.getUrlFromParams(json_dict, url_api)
+        # parsed_dict = self.parseInputValues(json_dict)
+        status_code, file_type, file_content = self.runQuery()
+
+
+        if assert_used:
+            if "expected code" in json_dict:
+                assert json_dict["expected code"] == status_code, "Status codes is different than expected. Test: {0}. URL: {1}.".format(json_dict["title"], self.query)
+            if "expected file" in json_dict:
+                assert json_dict["expected file"] == file_type, "Different file type returned than expected. Test: {0}. URL: {1}.".format(json_dict["title"], self.query)
+            ### BEGIN TESTING FILE CONTENTS:
+            file_content = self.renameKeysToStandard(file_content)
+            file_content = self.renameValsToStandard(file_content)
+            json_dict = self.parseInputValues(json_dict)
+            # IF used in url, IF contained in file's content, check if they match
+            if "Platform" in json_dict and "Platform" in file_content:
+                for platform in file_content["Platform"]:
+                    assert platform in json_dict["Platform"], "Platform declared, but not found in file. Test: {0}".format(json_dict["title"])
+            if "absoluteOrbit" in json_dict and "absoluteOrbit" in file_content:
+                for orbit in file_content["absoluteOrbit"]:
+                    assert int(orbit) in json_dict["absoluteOrbit"], "absoluteOrbit declared, but not found in file. Test: {0}".format(json_dict["title"])
+        # Else they didn't assert anything:
+        else:
+            print()
+            print("URL: {0}".format(self.query))
+            print("Status Code: {0}. File returned: {1}".format(status_code, file_type))
+            print()
+
+    def getUrlFromParams(self, json_dict, url_api):
         # DONT add these to url. (Used for tester). Add ALL others to allow testing keywords that don't exist
-        reserved_keys = ["expected file","expected code", "title"]
+        reserved_keywords = ["title", "print"]
+        asserts_keywords = ["expected file","expected code", "expected in file"]
+
+        assert_used = 0 != len([k for k,_ in json_dict.items() if k in asserts_keywords])
         keywords = []
         for key,val in json_dict.items():
             # If it's reserved, move on:
-            if key in reserved_keys:
+            if key in reserved_keywords or key in asserts_keywords:
                 continue
             # IF val is None, just add the key. Else add "key=val"
-            if val == "None":
+            if val == None:
                 keywords.append(str(key))
             # If you're testing multiple SAME params, add each key-val pair:
             elif isinstance(val, type([])):
                 keywords.append(str(key)+"="+",".join(val))
             else:
                 keywords.append(str(key)+"="+str(val))
-        self.query = url_api + "&".join(keywords)
-        status_code, returned_file = self.runQuery()
+        query = url_api + "&".join(keywords)
+        return query, assert_used
 
-        if "expected code" in json_dict:
-            assert json_dict["expected code"] == status_code, "Status codes is different than expected. Test: {0}. URL: {1}.".format(json_dict["title"], self.query)
-        if "expected file" in json_dict:
-            assert json_dict["expected file"] == returned_file, "Different file type returned than expected. Test: {0}. URL: {1}.".format(json_dict["title"], self.query)
 
-        if "expected file" not in json_dict and "expected code" not in json_dict:
-            print()
-            print("URL: {0}".format(self.query))
-            print("Status Code: {0}. File returned: {1}".format(status_code, returned_file))
-            print()
+    def parseInputValues(self, json_test):
+        # Turn string values to lists:
+        mutatable_dict = deepcopy(json_test)
+        try:
+            for key, val in json_test.items():
+                if key == "absoluteOrbit":
+                    mutatable_dict["absoluteOrbit"] = Input.parse_int_or_range_list(val)
+                elif key == "platform":
+                    mutatable_dict["Platform"] = Input.parse_string_list(val)
+                # elif key == "asfframe":
+        except ValueError as e:
+            assert False, "Test: {0}. Incorrect parameter: {1}".format(json_test["title"], str(e))
+        json_test = mutatable_dict
+        # Make each possible value line up with what the files returns:
+        json_test = self.renameValsToStandard(json_test)
+        return json_test
 
+    def renameKeysToStandard(self, json_dict):
+        ### absoluteOrbit:
+        if "Orbit" in json_dict:
+            json_dict["absoluteOrbit"] = json_dict.pop("Orbit")
+        return json_dict
+
+    # assumes values are in the form of {key: [value1,value2]}
+    def renameValsToStandard(self, json_dict):
+        if "Platform" in json_dict:
+            for i, platform in enumerate(json_dict["Platform"]):
+                # ALOS
+                if platform in ["ALOS", "A3"]:
+                    json_dict["Platform"][i] = "ALOS"
+                # AIRSAR
+                elif platform in ["AIRSAR", "AS"]:
+                    json_dict["Platform"][i] = "AIRSAR"
+                # ERS
+                elif platform in ["ERS"]:
+                    json_dict["Platform"][i] = "ERS"
+                # ERS-1
+                elif platform in ["ERS-1", "E1"]:
+                    json_dict["Platform"][i] = "ERS-1"
+                # ERS-2
+                elif platform in ["ERS-2", "E2"]:
+                    json_dict["Platform"][i] = "ERS-2"
+                # JERS-1
+                elif platform in ["JERS-1", "J1"]:
+                    json_dict["Platform"][i] = "JERS-1"
+                # RADARSAT-1
+                elif platform in ["RADARSAT-1", "R1"]:
+                    json_dict["Platform"][i] = "RADARSAT-1"
+                # SEASAT
+                elif platform in ["SEASAT", "SS"]:
+                    json_dict["Platform"][i] = "SEASAT"
+                # Sentinel-1:
+                elif platform in ["S1", "Sentinel-1", "Sentinel"]:
+                    json_dict["Platform"][i] = "Sentinel-1"
+                # Sentinel-1A
+                elif platform in ["Sentinel-1A", "SA"]:
+                    json_dict["Platform"][i] = "Sentinel-1A"
+                # Sentinel-1B
+                elif platform in ["Sentinel-1B", "SB"]:
+                    json_dict["Platform"][i] = "Sentinel-1B"
+                # SMAP
+                elif platform in ["SMAP", "SP"]:
+                    json_dict["Platform"][i] = "SMAP"
+                # UAVSAR
+                elif platform in ["UAVSAR", "UA"]:
+                    json_dict["Platform"][i] = "UAVSAR"
+        return json_dict
 
     def runQuery(self):
+        def csvToDict(file_content):
+            file_content = csv.reader(StringIO(file_content), delimiter=',')
+            file_content = [a for a in file_content]
+            # Rotate it counter-clockwise, so that row[0] == key of csv. (based on https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python)
+            rotated_content = list(map(type([]), zip(*file_content)))
+            file_content = {}
+            for column in rotated_content:
+                file_content[column[0]] = column[1:]
+            return file_content
+
+        def jsonToDict(json_data):
+            # Combine all matching key-value pairs, to-> key: [list of vals]
+            file_content = {}
+            for result in json_data:
+                for key,val in result.items():
+                    # Break apart nested lists if needed, (alows to forloop val):
+                    val = [val] if not isinstance(val, type([])) else val
+                    if key in file_content:
+                        for inner_val in val:
+                            file_content[key].append(inner_val)
+                    else:
+                        file_content[key] = []
+                        for inner_val in val:
+                            file_content[key].append(inner_val)
+            return file_content
+
         h = requests.head(self.query)
         # text/csv; charset=utf-8
         content_type = h.headers.get('content-type').split('/')[1]
@@ -59,7 +181,8 @@ class RunSingleURLFromFile():
                 content_type = "blank count"
         ## CSV
         elif content_type == "csv":
-            blank_csv = '"Granule Name","Platform","Sensor","Beam Mode","Beam Mode Description","Orbit","Path Number","Frame Number","Acquisition Date","Processing Date","Processing Level","Start Time","End Time","Center Lat","Center Lon","Near Start Lat","Near Start Lon","Far Start Lat","Far Start Lon","Near End Lat","Near End Lon","Far End Lat","Far End Lon","Faraday Rotation","Ascending or Descending?","URL","Size (MB)","Off Nadir Angle","Stack Size","Baseline Perp.","Doppler","GroupID"\n'
+            file_content = csvToDict(file_content)
+            blank_csv = {"Granule Name": [],"Platform": [],"Sensor": [],"Beam Mode": [],"Beam Mode Description": [],"Orbit": [],"Path Number": [],"Frame Number": [],"Acquisition Date": [],"Processing Date": [],"Processing Level": [],"Start Time": [],"End Time": [],"Center Lat": [],"Center Lon": [],"Near Start Lat": [],"Near Start Lon": [],"Far Start Lat": [],"Far Start Lon": [],"Near End Lat": [],"Near End Lon": [],"Far End Lat": [],"Far End Lon": [],"Faraday Rotation": [],"Ascending or Descending?": [],"URL": [],"Size (MB)": [],"Off Nadir Angle": [],"Stack Size": [],"Baseline Perp.": [],"Doppler": [],"GroupID":[]}
             if file_content == blank_csv:
                 content_type = "blank csv"
         ## DOWNLOAD / PLAIN
@@ -77,7 +200,8 @@ class RunSingleURLFromFile():
         elif content_type == "geojson":
             if file_content == '{\n  "features": [],\n  "type": "FeatureCollection"\n}':
                 content_type = "empty geojson"
-        ## JSON / JSONLITE
+
+        ## JSON / JSONLITE / ERROR
         elif content_type == "json":
             file_content = json.loads(file_content)
             ## ERROR
@@ -85,13 +209,21 @@ class RunSingleURLFromFile():
                 content_type = "error json"
             ## JSONLITE
             elif "results" in file_content:
-                if len(file_content["results"]) > 0:
-                    content_type = "jsonlite"
-                else:
+                file_content = file_content["results"]
+                if len(file_content) == 0:
                     content_type = "blank jsonlite"
+                else:
+                    content_type = "jsonlite"
+                    file_content = jsonToDict(file_content)
             ## JSON
-            elif file_content == [[]]:
-                content_type = "blank json"
+            else:
+                json_data = file_content[0]
+                if json_data == []:
+                    content_type = "blank json"
+                else:
+                    # dictonary-ify file_content to expected format:
+                    file_content = jsonToDict(json_data)
+
         ## KML
         elif content_type == "vnd.google-earth.kml+xml":
             content_type = "kml"
@@ -104,8 +236,7 @@ class RunSingleURLFromFile():
             blank_metalink = '<?xml version="1.0"?>\n<metalink xmlns="http://www.metalinker.org/" version="3.0">\n<publisher><name>Alaska Satellite Facility</name><url>http://www.asf.alaska.edu/</url></publisher>\n<files>\n</files>\n</metalink>'.replace(" ","")
             if file_content.replace(" ","") == blank_metalink:
                 content_type = "blank metalink"
-
-        return h.status_code, content_type
+        return h.status_code, content_type, file_content
 
 
 
@@ -141,6 +272,8 @@ def test_EachURLInYaml(json_test, get_cli_args):
     if only_run_cli != None and only_run_cli not in test_info["title"]:
         pytest.skip("Title of test did not match --only-run param")
 
+
+    # Default to command line if they used it, else check in the file. (can still be None)
     api_url = api_cli if api_cli != None else file_config['api']
     # Change any keywords for api to the api's url itself:
     api_url = helpers.getAPI(api_url, default="TEST", params="/services/search/param?")
