e4f8ddbc4baabcccf58f2ef33461f1878004bcad
Christy Fleming <ccfleming@alaska.edu>
2019-11-08 11:59:48 -0900

    Added granule_list & groupid to file verification

--- test/test_URLs.py
+++ test/test_URLs.py
@@ -41,8 +41,12 @@ class RunSingleURLFromFile():
                         # print(json.dumps(json_dict, indent=4, default=str))
                         # IF used in url, IF contained in file's content, check if they match
                         def checkFileContainsExpected(key, url_dict, file_dict):
+                            print("HERE?")
+                            # print(url_dict)
+                            print(file_dict)
                             if key in url_dict and key in file_dict:
                                 found_in_list = False
+                                print("HITTTTT")
                                 for found_param in file_dict[key]:
                                     for poss_list in url_dict[key]:
                                         if isinstance(poss_list, type([])):
@@ -61,9 +65,11 @@ class RunSingleURLFromFile():
                                         break
                                 assert found_in_list, key + " declared, but not found in file. Test: {0}".format(json_dict["title"])
                         
-                        checkFileContainsExpected("Platform", json_dict, file_content)
-                        checkFileContainsExpected("absoluteOrbit", json_dict, file_content)
-                        checkFileContainsExpected("asfframe", json_dict, file_content)
+                        # checkFileContainsExpected("Platform", json_dict, file_content)
+                        # checkFileContainsExpected("absoluteOrbit", json_dict, file_content)
+                        # checkFileContainsExpected("asfframe", json_dict, file_content)
+                        # checkFileContainsExpected("granule_list", json_dict, file_content)
+                        checkFileContainsExpected("groupid", json_dict, file_content)
 
         # If print wasn't declared, it gets set in parseTestValues:
         if json_dict["print"] == True:
@@ -112,6 +118,12 @@ class RunSingleURLFromFile():
                 elif key.lower() in ["frame", "asfframe"]:
                     del mutatable_dict[key]
                     mutatable_dict["asfframe"] = Input.parse_int_or_range_list(val)
+                elif key.lower() == "granule_list":
+                    del mutatable_dict[key]
+                    mutatable_dict["granule_list"] = Input.parse_string_list(val)
+                elif key.lower() == "groupid":
+                    del mutatable_dict[key]
+                    mutatable_dict["groupid"] = Input.parse_string_list(val)
 
         except ValueError as e:
             assert False, "Test: {0}. Incorrect parameter: {1}".format(json_test["title"], str(e))
@@ -128,7 +140,16 @@ class RunSingleURLFromFile():
         for key in ["frame", "frameNumber", "Frame Number"]:
             if key in json_dict:
                 json_dict["asfframe"] = json_dict.pop(key)
+        print(json_dict)
+        ### granule_list:
+        for key in ["granuleName", "Granule Name"]:
+            if key in json_dict:
+                json_dict["granule_list"] = json_dict.pop(key)
+        ### groupid:
+        if "groupID" in json_dict:
+            json_dict["groupid"] = json_dict.pop("groupID")
         return json_dict
+
 
     # assumes values are in the form of {key: [value1,value2]}
     def renameValsToStandard(self, json_dict):
