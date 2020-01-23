cf14a3978466f41a94e29fba46223c4e560fc5be
Gregory Short <glshort@alaska.edu>
2020-01-23 14:35:01 -0600

    Add stress tester script and input file

---
+++ test/stress_test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import argparse
+import re
+import requests
+import multiprocessing
+import random
+import time
+
+class bulk_query:
+    def __init__(self, file, save, parallel):
+        self.file = file
+        self.save = save
+        self.parallel = parallel
+        self.queries = None
+
+    def read_queries(self):
+        try:
+            with open(self.file, 'r') as query_file:
+                self.queries = []
+                for q in query_file.read().splitlines():
+                    q = q.strip()
+                    if len(q) <= 0 or q[0] == '#':
+                        continue
+                    p = re.compile('\&?output\=\w+', re.IGNORECASE)
+                    q = p.sub('', q)
+                    q = q + '&output=jsonlite'
+                    self.queries.append(q)
+        except IOError as ioe:
+            print(ioe)
+            exit()
+        random.shuffle(self.queries)
+        return
+
+    def run_query(self, q, s=None):
+        if s is None:
+            s = requests.Session()
+        start = time.perf_counter()
+        r = s.get(q)
+        end = time.perf_counter()
+        result = ''
+        if r.status_code != 200:
+            result = '"{0}","ERROR: HTTP status {1}""'.format(q, r.status_code)
+        else:
+            result = '"{0}","{1}"'.format(q, end - start)
+        output_lock.acquire()
+        print(result)
+        with open(self.save, 'a') as save_file:
+            save_file.write('{0}\n'.format(result))
+        output_lock.release()
+        return result
+
+    def run_queries(self):
+        pool = multiprocessing.Pool(self.parallel)
+
+        with open(self.save, 'w') as save_file:
+            save_file.write('"API Query","Duration (s)"\n')
+        s = requests.Session()
+        self.done(*pool.map(self.run_query, self.queries))
+
+    def done(self, *whateva):
+        pass
+
+if __name__ == '__main__':
+    output_lock = multiprocessing.Lock()
+    parser = argparse.ArgumentParser(description='Read API query URLs from a file, test them, and gather statistics.')
+    parser.add_argument('-f', '--file', action='store', required=True, help='File to read query URLs from')
+    parser.add_argument('-s', '--save', action='store', required=True, help='File to save results to')
+    parser.add_argument('-p', '--parallel', action='store', type=int, required=True, help='Number of queries to run in parallel')
+    args = parser.parse_args()
+    querier = bulk_query(file=args.file, save=args.save, parallel=args.parallel)
+    querier.read_queries()
+    querier.run_queries()
+    querier.done()
