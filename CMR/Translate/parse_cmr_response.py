c443309b473abfa72c88214feff24dcefa85911d
William Horn <wbhorn@alaska.edu>
2020-02-05 12:47:17 -0900

    Create get_attr helper function that wraps get_val

--- CMR/Translate/parse_cmr_response.py
+++ CMR/Translate/parse_cmr_response.py
@@ -34,9 +34,13 @@ def parse_granule(granule):
         else:
             return default
 
-    frame_number = get_val(attr('FRAME_NUMBER'), default='NA') \
-        if get_val(attr('ASF_PLATFORM'), default='NA') in ['Sentinel-1A', 'Sentinel-1B', 'ALOS'] \
-        else get_val(attr('CENTER_ESA_FRAME'), default='NA')
+    def get_attr(path, default=None):
+        return get_val(attr(path), default=default)
+
+    asf_platforms = ['Sentinel-1A', 'Sentinel-1B', 'ALOS']
+    frame_number = get_attr('FRAME_NUMBER', default='NA') \
+        if get_attr('ASF_PLATFORM', default='NA') in asf_platforms \
+        else get_attr('CENTER_ESA_FRAME', default='NA')
 
     result = {
         'granuleName': get_val("./DataGranule/ProducerGranuleId"),
@@ -46,59 +50,62 @@ def parse_granule(granule):
         'stopTime':  get_val("./Temporal/RangeDateTime/EndingDateTime"),
         'absoluteOrbit': get_val(
             "./OrbitCalculatedSpatialDomains/OrbitCalculatedSpatialDomain/OrbitNumber"),
-        'platform': get_val(attr('ASF_PLATFORM'), default=get_val("./Platforms/Platform/ShortName")),
-        'md5sum': get_val(attr('MD5SUM'), default='NA'),
-        'beamMode': get_val(attr('BEAM_MODE_TYPE'), default='NA'),
-        'configurationName': get_val(attr('BEAM_MODE_DESC'), default='NA'),
-        'bytes': get_val(attr("BYTES"), default='NA'),
-        'granuleType':  get_val(attr('GRANULE_TYPE'), default='NA'),
-        'sceneDate': get_val(attr('ACQUISITION_DATE'), default='NA'),
-        'flightDirection': get_val(attr('ASCENDING_DESCENDING'), default='NA'),
-        'thumbnailUrl': get_val(attr('THUMBNAIL_URL'), default='NA'),
-        'farEndLat':  get_val(attr('FAR_END_LAT'), default='NA'),
-        'farStartLat':  get_val(attr('FAR_START_LAT'), default='NA'),
-        'nearStartLat':  get_val(attr('NEAR_START_LAT'), default='NA'),
-        'nearEndLat':  get_val(attr('NEAR_END_LAT'), default='NA'),
-        'farEndLon':  get_val(attr('FAR_END_LON'), default='NA'),
-        'farStartLon':  get_val(attr('FAR_START_LON'), default='NA'),
-        'nearStartLon':  get_val(attr('NEAR_START_LON'), default='NA'),
-        'nearEndLon':  get_val(attr('NEAR_END_LON'), default='NA'),
-        'processingType':  get_val(attr('PROCESSING_LEVEL'), default='NA'),
-        'finalFrame':  get_val(attr('CENTER_ESA_FRAME'), default='NA'),
-        'centerLat':  get_val(attr('CENTER_LAT'), default='NA'),
-        'centerLon':  get_val(attr('CENTER_LON'), default='NA'),
-        'polarization':  get_val(attr('POLARIZATION'), default='NA'),
-        'faradayRotation':  get_val(attr('FARADAY_ROTATION'), default='NA'),
+        'platform': get_attr(
+            'ASF_PLATFORM',
+            default=get_val("./Platforms/Platform/ShortName")
+        ),
+        'md5sum': get_attr('MD5SUM', default='NA'),
+        'beamMode': get_attr('BEAM_MODE_TYPE', default='NA'),
+        'configurationName': get_attr('BEAM_MODE_DESC', default='NA'),
+        'bytes': get_attr("BYTES", default='NA'),
+        'granuleType':  get_attr('GRANULE_TYPE', default='NA'),
+        'sceneDate': get_attr('ACQUISITION_DATE', default='NA'),
+        'flightDirection': get_attr('ASCENDING_DESCENDING', default='NA'),
+        'thumbnailUrl': get_attr('THUMBNAIL_URL', default='NA'),
+        'farEndLat':  get_attr('FAR_END_LAT', default='NA'),
+        'farStartLat':  get_attr('FAR_START_LAT', default='NA'),
+        'nearStartLat':  get_attr('NEAR_START_LAT', default='NA'),
+        'nearEndLat':  get_attr('NEAR_END_LAT', default='NA'),
+        'farEndLon':  get_attr('FAR_END_LON', default='NA'),
+        'farStartLon':  get_attr('FAR_START_LON', default='NA'),
+        'nearStartLon':  get_attr('NEAR_START_LON', default='NA'),
+        'nearEndLon':  get_attr('NEAR_END_LON', default='NA'),
+        'processingType':  get_attr('PROCESSING_LEVEL', default='NA'),
+        'finalFrame':  get_attr('CENTER_ESA_FRAME', default='NA'),
+        'centerLat':  get_attr('CENTER_LAT', default='NA'),
+        'centerLon':  get_attr('CENTER_LON', default='NA'),
+        'polarization':  get_attr('POLARIZATION', default='NA'),
+        'faradayRotation':  get_attr('FARADAY_ROTATION', default='NA'),
         'stringFootprint': wkt_shape,
-        'doppler': get_val(attr('DOPPLER'), default='NA'),
-        'baselinePerp': get_val(attr('INSAR_BASELINE'), default='NA'),
-        'insarStackSize': get_val(attr('INSAR_STACK_SIZE'), default='NA'),
-        'processingDescription': get_val(attr('PROCESSING_DESCRIPTION'), default='NA'),
-        'percentTroposphere': 'NA', # not in CMR
+        'doppler': get_attr('DOPPLER', default='NA'),
+        'baselinePerp': get_attr('INSAR_BASELINE', default='NA'),
+        'insarStackSize': get_attr('INSAR_STACK_SIZE', default='NA'),
+        'processingDescription': get_attr('PROCESSING_DESCRIPTION', default='NA'),
+        'percentTroposphere': 'NA',  # not in CMR
         'frameNumber': frame_number,
-        'percentCoherence': 'NA', # not in CMR
+        'percentCoherence': 'NA',  # not in CMR
         'productName': get_val("./DataGranule/ProducerGranuleId"),
-        'masterGranule': 'NA', # almost always None in API
-        'percentUnwrapped': 'NA', # not in CMR
-        'beamSwath': 'NA', # .......complicated
-        'insarGrouping': get_val(attr('INSAR_STACK_ID'), default='NA'),
-        'offNadirAngle': get_val(attr('OFF_NADIR_ANGLE'), default='NA'),
-        'missionName': get_val(attr('MISSION_NAME'), default='NA'),
-        'relativeOrbit': get_val(attr('PATH_NUMBER'), default='NA'),
-        'flightLine': get_val(attr('FLIGHT_LINE'), default='NA'),
-        'processingTypeDisplay': get_val(attr('PROCESSING_TYPE_DISPLAY'), default='NA'),
-        'track': get_val(attr('PATH_NUMBER'), default='NA'),
-        'beamModeType': get_val(attr('BEAM_MODE_TYPE'), default='NA'),
-        'processingLevel': get_val(attr('PROCESSING_TYPE'), default='NA'),
-        'lookDirection': get_val(attr('LOOK_DIRECTION'), default='NA'),
-        'varianceTroposphere': 'NA', # not in CMR
-        'slaveGranule': 'NA', # almost always None in API
+        'masterGranule': 'NA',  # almost always None in API
+        'percentUnwrapped': 'NA',  # not in CMR
+        'beamSwath': 'NA',  # .......complicated
+        'insarGrouping': get_attr('INSAR_STACK_ID', default='NA'),
+        'offNadirAngle': get_attr('OFF_NADIR_ANGLE', default='NA'),
+        'missionName': get_attr('MISSION_NAME', default='NA'),
+        'relativeOrbit': get_attr('PATH_NUMBER', default='NA'),
+        'flightLine': get_attr('FLIGHT_LINE', default='NA'),
+        'processingTypeDisplay': get_attr('PROCESSING_TYPE_DISPLAY', default='NA'),
+        'track': get_attr('PATH_NUMBER', default='NA'),
+        'beamModeType': get_attr('BEAM_MODE_TYPE', default='NA'),
+        'processingLevel': get_attr('PROCESSING_TYPE', default='NA'),
+        'lookDirection': get_attr('LOOK_DIRECTION', default='NA'),
+        'varianceTroposphere': 'NA',  # not in CMR
+        'slaveGranule': 'NA',  # almost always None in API
         'sensor': get_val('./Platforms/Platform/Instruments/Instrument/ShortName'),
         'fileName': get_val("./OnlineAccessURLs/OnlineAccessURL/URL").split('/')[-1],
         'downloadUrl': get_val("./OnlineAccessURLs/OnlineAccessURL/URL"),
-        'browse': get_browse_urls(granule.xpath('./AssociatedBrowseImageUrls')),
+        'browse': get_browse_urls(granule, './AssociatedBrowseImageUrls'),
         'shape': shape,
-        'sarSceneId': 'NA', # always None in API
+        'sarSceneId': 'NA',  # always None in API
         'product_file_id': get_val("./GranuleUR"),
         'sceneId': get_val("./DataGranule/ProducerGranuleId"),
         'firstFrame': get_val(attr('CENTER_ESA_FRAME'), default='NA'),
@@ -165,10 +172,11 @@ def shape_not_closed(shapes):
     )
 
 
-def get_browse_urls(browseElems):
+def get_browse_urls(granule, browse_path):
+    browse_elems = granule.xpath(browse_path)
     browseList = []
 
-    for b in browseElems:
+    for b in browse_elems:
         browseList.extend(b.findall('ProviderBrowseUrl'))
 
     browseUrls = [''.join(b.itertext()).strip() for b in browseList]
