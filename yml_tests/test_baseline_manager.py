9ff5c7f9952d59d5f17df9541fc23898473624b2
Christy Fleming <ccfleming@alaska.edu>
2020-04-07 16:05:07 -0800

    WIP Adding flag for maturity param

--- yml_tests/test_baseline_manager.py
+++ yml_tests/test_baseline_manager.py
@@ -22,6 +22,10 @@ class test_baseline():
             assert False, "Endpoint test ran, but '--api' not declared in CLI (test_files_to_wkt).\nCan also add 'default' api to use in yml_tests/pytest_config.yml.\n"
         url_parts = [test_api, test_vars["endpoint"]]
         full_url = '/'.join(s.strip('/') for s in url_parts) # If both/neither have '/' between them, this still joins them correctly
+        if "maturity" in test_info:
+            if cli_args['api'].lower() != 'prod':
+                url_parts += 'maturity=' + test_info['maturity']
+
 
         # Get the url string and (bool)if assert was used:
         keywords, assert_used = self.getKeywords(test_info)
