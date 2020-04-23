fed912afad1b1a5aad4621a814bdcd2a83269b58
Cameron Showalter <cjshowalter@alaska.edu>
2020-04-23 10:45:25 -0800

    Switched to new error formats. Removed the key errors from API returning unexpected results that I could find. Better error reporting

--- yml_tests/test_dateParser_manager.py
+++ yml_tests/test_dateParser_manager.py
@@ -4,12 +4,13 @@ from datetime import datetime
 
 class test_date_parser():
     def __init__(self, test_info, file_conf, cli_args, test_vars):
+        self.error_msg = "Reason: {0}\n - File: '{1}'\n - Test: '{2}'".format('{0}', file_conf["yml name"], test_info["title"])
         if cli_args["api"] != None:
             test_api = cli_args["api"]
         elif "api" in file_conf:
             test_api = file_conf["api"]
         else:
-            assert False, "Endpoint test ran, but '--api' not declared in CLI (test_files_to_wkt).\nCan also add 'default' api to use in yml_tests/pytest_config.yml.\n"
+            assert False, self.error_msg.format("Endpoint test ran, but '--api' not declared in CLI. You can also add 'default' api to use in yml_tests/pytest_config.yml, or add 'api: *url*' to each test.")
 
         url_parts = [test_api, test_vars["endpoint"]]
         self.full_url = '/'.join(s.strip('/') for s in url_parts) # If both/neither have '/' between them, this still joins them correctly
@@ -25,7 +26,12 @@ class test_date_parser():
         r = requests.get(self.full_url)
         h = requests.head(self.full_url)
         content = r.content.decode("utf-8")
-        content_type = h.headers.get('content-type').split("/")[1]
+        content_header = h.headers.get('content-type')
+        try:
+            content_type = content_header.split("/")[1]
+        except AttributeError:
+            assert False, self.error_msg.format("Header is not formatted as expected. Header: {0}.\nFile Content (First 500 char): \n{1}\n".format(content_header, content[:500]))
+
 
         if content_type == "json":
             content = json.loads(content)
@@ -47,18 +53,18 @@ class test_date_parser():
 
     def runAssertTests(self, status_code, content_type, content):
         if content_type == "html" or status_code >= 500:
-            assert False, "API returned error page. Test: '{0}'. URL: '{1}. \nHTML:\n{2}\n".format(self.test_info['title'], self.full_url, content)
+            assert False, self.error_msg.format("API returned error page. \nHTML (First 500 char):\n{0}\n".format(content[:500]))
         if "expected error" in self.test_info:
             if "errors" in content:
-                assert self.test_info["expected error"].lower() in str(content).lower(), "API returned a different error than expected. Test: '{0}'. URL: '{1}'.".format(self.test_info["title"], self.full_url)
+                assert self.test_info["expected error"].lower() in str(content).lower(), self.error_msg.format("API returned a different error than expected.")
             else:
-                assert False, "API parsed value when validation error expected. Test: '{0}'. URL: '{1}'.".format(self.test_info["title"], self.full_url)
+                assert False, self.error_msg.format("API parsed value when validation error expected.")
         if "expected date" in self.test_info:
             if "date" in content:
                 try:
-                    time = datetime.strptime(content["date"]["parsed"], "%Y-%m-%dT%H:%M:%SZ")
-                except ValueError as e:
-                    assert False, "API did not return the a date. Error Message: '{0}'' Test: '{1}'. URL: '{2}'.".format(str(e), self.test_info["title"], self.full_url)
+                    datetime.strptime(content["date"]["parsed"], "%Y-%m-%dT%H:%M:%SZ")
+                except (ValueError, TypeError) as e:
+                    assert False, self.error_msg.format("API did not return a date. Error Message: '{0}'.".format(str(e)))
             else:
-                assert False, "API returned an unexpected parsing error. Test: '{0}'. URL: '{1}'.".format(self.test_info["title"], self.full_url)
+                assert False, self.error_msg.format("API did not return a date. Returned (First 500 char):\n{0}\n".format(content[:500]))
 
