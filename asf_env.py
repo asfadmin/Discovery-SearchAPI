5483bd4ed54248b341855e9b67da44de54bbf46e
Cameron Showalter <cjshowalter@alaska.edu>
2020-04-30 14:10:18 -0800

    Moved json out into it's own yaml

--- asf_env.py
+++ asf_env.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import yaml
 from flask import request
 
 def get_config():
@@ -7,7 +8,8 @@ def get_config():
         logging.warning('os.environ[\'MATURITY\'] not set! Defaulting to local config.]')
 
     maturity = os.environ['MATURITY'] if 'MATURITY' in os.environ.keys() else 'local'
-    maturities = get_maturities()
+    with open("maturities.yml", "r") as yml_file:
+        maturities = yaml.safe_load(yml_file)
 
     # need to inject this into the temporary maturity for crossover to prod so it still accepts the 'maturity' param
     flex_maturity = maturities[maturity]['flexible_maturity']
@@ -17,100 +19,7 @@ def get_config():
 
     config = maturities[maturity]
     config['flexible_maturity'] = flex_maturity
-
+    
     return config
 
-def get_maturities():
-    return {
-        'local': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': None,
-            'this_api': 'http://127.0.0.1:5000',
-            'cmr_base': 'https://cmr.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': True
-        },
-        'devel': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-4',
-            'this_api': 'https://api-dev.asf.alaska.edu',
-            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_alocal.asf.alaska.edusf'
-            },
-            'flexible_maturity': True
-        },
-        'devel-beanstalk': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-4',
-            'this_api': 'https://api-dev-beanstalk.asf.alaska.edu',
-            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': True
-        },
-        'test': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-3',
-            'this_api': 'https://api-test.asf.alaska.edu',
-            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': True
-        },
-        'test-beanstalk': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-3',
-            'this_api': 'https://api-test-beanstalk.asf.alaska.edu',
-            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': True
-        },
-        'prod': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': None,
-            'this_api': 'https://api.daac.asf.alaska.edu',
-            'cmr_base': 'https://cmr.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': False
-        },
-        'prod-private': {
-            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
-            'analytics_id': 'UA-118881300-5',
-            'this_api': 'https://api-prod-private.asf.alaska.edu',
-            'cmr_base': 'https://cmr.earthdata.nasa.gov',
-            'cmr_health': '/search/health',
-            'cmr_api': '/search/granules.echo10',
-            'cmr_collections': '/search/collections',
-            'cmr_headers': {
-                'Client-Id': 'vertex_asf'
-            },
-            'flexible_maturity': False
-        }
-    }
+
