4945da37c24655f91e3b1fd11596329f118a7c2b
Cameron Showalter <cjshowalter@alaska.edu>
2022-01-20 15:37:25 -0900

    Adding setup.py, to install the SearchAPI package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setuptools.setup(
+    name="SearchAPI",
+    author="Alaska Satellite Facility Discovery Team",
+    author_email="uaf-asf-discovery@alaska.edu",
+    description="Helper package to run the API.",
+    long_description=readme,
+    url="https://github.com/asfadmin/Discovery-SearchAPI.git",
+    packages=setuptools.find_packages(),
+    package_dir={"SearchAPI": "SearchAPI"},   
+)
