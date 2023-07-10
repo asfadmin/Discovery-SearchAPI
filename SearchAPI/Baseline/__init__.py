6b460b3de33ae485faed7e73d049b8c832b2b863
kim <ktfairbanks@alaska.edu>
2023-07-10 08:43:51 -0800

    changes "build_stack_params" to "build_stack_query"

--- SearchAPI/Baseline/__init__.py
+++ SearchAPI/Baseline/__init__.py
@@ -1,4 +1,4 @@
 from .Stack import get_stack
 from .Stack import get_default_product_type
-from .Stack import build_stack_params
+from .Stack import build_stack_query
 from .Calc import calculate_perpendicular_baselines
