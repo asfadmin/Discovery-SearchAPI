e39bda5dd53d26544aee7dbd343e7c5407c74a91
kim <ktfairbanks@alaska.edu>
2023-11-02 11:14:54 -0800

    adds dataset constants and 'datasets' searchable parameter, for use by Vertex's dataset based searches

--- SearchAPI/CMR/Translate/__init__.py
+++ SearchAPI/CMR/Translate/__init__.py
@@ -3,3 +3,4 @@ from .parse_cmr_response import parse_cmr_response
 from .translate_params import translate_params
 from .input_fixer import input_fixer
 from .fields import get_field_paths
+from .datasets import platform_datasets
