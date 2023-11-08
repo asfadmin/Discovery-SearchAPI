e1ec96b2bc177d5b0a8af8cc9167b46e4071a333
kim <ktfairbanks@alaska.edu>
2023-11-08 09:59:31 -0900

    changes 'datasets' keyword to 'dataset'

--- SearchAPI/CMR/Translate/input_map.py
+++ SearchAPI/CMR/Translate/input_map.py
@@ -56,8 +56,8 @@ def input_map():
         'relativeburstid':      ['attribute[]',             'int,BURST_ID_RELATIVE,{0}',        parse_int_list],
         'absoluteburstid':      ['attribute[]',             'int,BURST_ID_ABSOLUTE,{0}',        parse_int_list],
         'fullburstid':          ['attribute[]',             'string,BURST_ID_FULL,{0}',         parse_string_list],
-        'operaburstid':              ['attribute[]',             'string,OPERA_BURST_ID,{0}',              parse_string_list],
-        'datasets':             [None,                      '{0}',                              parse_string_list]
+        'operaburstid':         ['attribute[]',             'string,OPERA_BURST_ID,{0}',        parse_string_list],
+        'dataset':              [None,                      '{0}',                              parse_string_list]
     }
 
     return parameter_map
